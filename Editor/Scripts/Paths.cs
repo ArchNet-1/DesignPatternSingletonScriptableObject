35fa5487e28f793d4f90f25bd5898f5f791578b5
Archaons <louispakel@gmail.com>
2021-03-29 22:41:14 +0200

    Create Paths.cs

---
+++ Editor/Scripts/Paths.cs
@@ -0,0 +1,12 @@
+﻿namespace ArchNet.DesignPattern.SingletonScriptableObject
+{
+    /// <summary>
+    /// Description : Paths for get a specific asset for the editor service
+    /// @author Louis PAKEL
+    /// </summary>
+    public static class Paths
+    {
+        public const string PopupService = "Assets/Views/OnBoarding/Helpers/Service Popup/PopupService.asset";
+        public const string OnBoardingService = "Assets/Views/OnBoarding/OnBoardingService.asset";
+    }
+}
